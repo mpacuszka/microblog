c192c2d5e2f506134ba82d787d20340b4d949837
Mateusz <mpacuszka@gmail.com>
2018-02-01 23:52:34 +0100

    Flask-WTF added changes in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "you-will-never-guess"
+# ... add more variables here as needed
 
 from app import routes
